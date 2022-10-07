5fe6a8e69e03cd82959fb75bf78f374422ae51c6
unknown <dhananjaywadhavane2016@gmail.com>
2022-10-07 11:06:53 +0530

    initial commit

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
+
+function App() {
+  const expenses = [
+    { 
+      id: 'e1',
+      title: 'Car Insurance', 
+      amount: 500, 
+      date: new Date(2022, 10, 3)
+    },
+    { 
+      id: 'e2',
+      title: 'Bike Insurance', 
+      amount: 100, 
+      date: new Date(2022, 10, 3)
+    },
+    { 
+      id: 'e3',
+      title: 'House Rent', 
+      amount: 3000, 
+      date: new Date(2022, 10, 3)
+    },
+    { 
+      id: 'e4',
+      title: 'Electricity Bill', 
+      amount: 300, 
+      date: new Date(2022, 10, 3)
+    }
+  ]
+  return (
+    <>
+      <Expenses/>
+
+    </>
+  );
+}
+
+export default App;
